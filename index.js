5cdf181f4a29dfab0c74697bfa01f81d76dc0482
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2022-09-18 15:41:46 +0000

    adjusted for heroku deployment

--- index.js
+++ index.js
@@ -145,8 +145,8 @@ client.on('messageCreate', message =>{
     }//closing brace for role check
 
 });
-
-client.login(process.env['TOKEN']);
+const bot_token = process.env['TOKEN']
+client.login(bot_token);
 
 //https://discord.js.org/#/docs/discord.js/main/general/welcome
 
