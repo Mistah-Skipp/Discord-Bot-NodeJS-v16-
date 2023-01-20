b3ce97077a1da08b8451f8d73e15ebc81cde5b11
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2023-01-20 13:07:29 -0500

    reverted es6

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 //const keepAlive = require("./keepAlive.js");
-import { Client, GatewayIntentBits, Options, Collection } from './node_modules/discord.js/src/index.js';
-import { readdirSync } from 'node:fs';
-import { join } from 'node:path';
+const { Client, GatewayIntentBits, Options, Collection} = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST } = require('@discordjs/rest');
 
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildPresences] });
 
 client.commands = new Collection();
-const commandsPath = join(__dirname, 'commands');
-const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const filePath = join(commandsPath, file);
+	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	client.commands.set(command.data.name, command);
 }
