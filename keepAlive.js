32109434ddc47ac81400d382303f8d9b4f281e30
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2023-01-20 15:40:49 -0500

    keepalive runs index

--- keepAlive.js
+++ keepAlive.js
@@ -1,16 +1,9 @@
-/*var http = require('http');
-
-http.createServer(function (req, res) {
-  res.write("I'm alive");
-  res.end();
-}).listen(8000);
-
-*/
+const bot = require ('./index.js');
 const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
+//app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/', bot);
 app.listen(port, () => console.log(`listening at ${port}`));
 
