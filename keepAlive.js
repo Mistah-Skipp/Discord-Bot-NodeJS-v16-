16f316e5be579a89d477df929a4b1a83fb2a51aa
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2023-01-20 15:51:03 -0500

    fix 4

--- keepAlive.js
+++ keepAlive.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-//app.get('/', (req, res) => res.send('Hello World!'));
-app.use('/', bot);
+app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`listening at ${port}`));
 
