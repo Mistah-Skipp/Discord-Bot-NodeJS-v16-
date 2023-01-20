b11c15d48ec530be3b053b61afa3b1b16ae28bf3
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2023-01-20 12:32:25 -0500

    started transfering from node to es6

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 //const keepAlive = require("./keepAlive.js");
-const { Client, GatewayIntentBits, Options, Collection} = require('discord.js');
-const fs = require('node:fs');
-const path = require('node:path');
-const { REST } = require('@discordjs/rest');
+import { Client, GatewayIntentBits, Options, Collection } from './node_modules/discord.js';
+import { readdirSync } from 'node:fs';
+import { join } from 'node:path';
 
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildPresences] });
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = join(__dirname, 'commands');
+const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
+	const filePath = join(commandsPath, file);
 	const command = require(filePath);
 	client.commands.set(command.data.name, command);
 }
